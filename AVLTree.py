f10f201573796153ccb6b32d1aade5920df2fd71
eitanadmoni <eitanadmoni@mail.tau.ac.il>
2023-05-14 18:55:01 +0300

    name+id Eitan

--- AVLTree.py
+++ AVLTree.py
@@ -1,8 +1,8 @@
 #username - complete info
 #id1      - complete info 
 #name1    - complete info 
-#id2      - complete info
-#name2    - complete info  
+#id2      - 323832949
+#name2    - Eitan Admoni
 
 
 
