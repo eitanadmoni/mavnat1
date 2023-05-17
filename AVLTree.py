e99cd0b07efff0a9d27d405fddae42e32bb10697
Yiftah <yiftahbaruch@mail.tau.ac.il>
2023-05-17 13:27:45 +0300

    Better roll function

--- AVLTree.py
+++ AVLTree.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 
 
 
+
 """A class represnting a node in an AVL tree"""
 
 class AVLNode(object):
@@ -316,7 +317,31 @@ class AVLTree(object):
 			current_node.set_BF(0)
 			current_node.set_left(AVLNode.create_virtual_node())
 			current_node.set_right(AVLNode.create_virtual_node())
-			return balance_after_insertion(current_node)
+			return AVLTree.balance_after_insertion(current_node)
+
+	@staticmethod
+	def basic_roll (node: AVLNode, is_left_roll=False):
+		rootParent = node.get_parent()
+		realGetLeft = AVLNode.get_left
+		realGetRight = AVLNode.get_right
+		realSetLeft = AVLNode.set_left
+		realSetRight = AVLNode.et_right
+		if is_left_roll:
+			realGetLeft = AVLNode.get_right
+			realGetRight = AVLNode.get_left
+			realSetLeft = AVLNode.get_right
+			realSetRight = AVLNode.get_left
+		leftChild = realGetLeft(node)
+		leftRightChild = realGetRight(node, leftChild)
+		realSetRight(node, leftRightChild)
+		node.update()
+		realSetRight(leftChild, node)
+		leftChild.update()
+		if node.is_right_child():
+			rootParent.set_right(leftChild)
+		else:
+			rootParent.set_left(leftChild)
+		rootParent.update()
 
 
 	"""Perform a basic roll operation
@@ -327,42 +352,17 @@ class AVLTree(object):
 	"""
 	@staticmethod
 	def roll (node: AVLNode, direction: int):
-		rootParent = node.get_parent()
-		isRightChild = node.is_right_child()
-		leftChild = node.get_left()
-		rightChild = node.get_left()
-		if direction == R: #right roll.
-			leftRightChild = leftChild.get_left()
-			node.set_left(leftRightChild)
-			node.update()
-			leftChild.set_right(node)
-			leftChild.update()
-			if isRightChild:
-				rootParent.set_right(leftChild)
-			else:
-				rootParent.set_left(leftChild)
-			rootParent.update()
+		if direction in (R, L):
+			left_roll = direction == L
+			AVLTree.basic_roll(node, is_left_roll=left_roll)
 			return 1
-		elif direction == L: #left roll.
-			rightLeftChild = rightChild.get_left()
-			node.set_right(rightLeftChild)
-			node.update()
-			rightChild.set_left(node)
-			rightChild.update()
-			if isRightChild:
-				rootParent.set_right(rightChild)
-			else:
-				rootParent.set_right(rightChild)
 		elif direction == RL:
 			AVLTree.roll(node.right, R)
 			AVLTree.roll(node, L)
-			return 2
 		else:
 			AVLTree.roll(node.left, L)
 			AVLTree.roll(node, R)
-			return 2
-		rootParent.update()
-		return 1
+		return 2
 
 
 
